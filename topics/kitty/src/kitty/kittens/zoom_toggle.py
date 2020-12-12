6615076628d493f86e68d0aa2cc2a01310fc747c
Manuel Sanchez Pinar <aomanu@gmail.com>
2020-12-12 10:43:09 +0100

    Fix zoom_toggle text

--- topics/kitty/src/kitty/kittens/zoom_toggle.py
+++ topics/kitty/src/kitty/kittens/zoom_toggle.py
@@ -3,16 +3,16 @@ def main(args):
 
 
 def handle_result(args, answer, target_window_id, boss):
-    zoom_txt = "[Zoom]"
+    zoom_txt = "[F]"
     tab = boss.active_tab
     if tab is not None:
         title = tab.title
         if tab.current_layout.name == "stack":
             tab.last_used_layout()
-            tab.set_title(title.strip(zoom_txt))
+            tab.set_title(title.replace(zoom_txt, ""))
         else:
             tab.goto_layout("stack")
-            tab.set_title(title + zoom_txt)
+            tab.set_title(zoom_txt + title)
 
 
 handle_result.no_ui = True
