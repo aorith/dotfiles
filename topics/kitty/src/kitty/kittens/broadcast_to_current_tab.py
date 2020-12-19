dfb2958a5a3b37c6834bb7b5b2196a2289857083
Manuel Sanchez Pinar <aomanu@gmail.com>
2020-12-19 19:47:08 +0100

    Broadcast implemented in 0.19.3

--- topics/kitty/src/kitty/kittens/broadcast_to_current_tab.py
+++ topics/kitty/src/kitty/kittens/broadcast_to_current_tab.py
@@ -1,25 +1,18 @@
-from sys import exit
+from kittens.tui.handler import result_handler
+
 
 def main(args):
-    # this is the main entry point of the kitten, it will be executed in
-    # the overlay window when the kitten is launched
-    print("Send to all windows in this Tab.\n")
-    try:
-        answer = input("> ")
-    except KeyboardInterrupt:
-        print("Cancelled.")
-        exit(0)
-    # whatever this function returns will be available in the
-    # handle_result() function
-    return answer
+    pass
 
 
+@result_handler(no_ui=True)
 def handle_result(args, answer, target_window_id, boss):
     tab = boss.active_tab
-    windows_dict = tab.list_windows(target_window_id)
-    for w_dict in windows_dict:
-        w = boss.window_id_map.get(w_dict.get("id"))
-        if w is not None:
-            #w.paste_bytes(answer + "\x0d")
-            w.paste(answer + "\r")
-
+    boss.launch(
+        "--allow-remote-control",
+        "--type=os-window",
+        "--title=Broadcast",
+        "/bin/bash",
+        "-lc",
+        f"kitty +kitten broadcast --match-tab=id:{tab.id}",
+    )
