deaf2324849f6c8017a631113ecc0629186482b4
Manuel Sanchez Pinar <aomanu@gmail.com>
2020-12-20 12:45:20 +0100

    Remove template.conf from available themes

--- topics/kitty/src/kitty/scripts/switch-theme.sh
+++ topics/kitty/src/kitty/scripts/switch-theme.sh
@@ -5,5 +5,5 @@ if [[ "$1" == "reset" ]]; then
     kitty @ set-colors --reset --all
 else
     cd ~/.config/kitty/themes || exit 1
-    find . -type f -path ./kitty-themes/.tools -prune -false -o -name "*conf" | fzf --preview "head -n 100 {} && kitty @ set-colors -a -c {}"
+    find . -type f -name "*.conf" -not -name "template.conf" | fzf --preview "head -n 100 {} && kitty @ set-colors -a -c {}"
 fi
