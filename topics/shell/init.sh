1b8dc8acdfc2828f03e838b619845cd3117f05cf
Manuel Sanchez Pinar <aomanu@gmail.com>
2020-12-27 14:28:58 +0100

    zsh: Refactor and clean ~/

--- topics/shell/init.sh
+++ topics/shell/init.sh
@@ -4,7 +4,11 @@
 create_link "${PWD}/src/bashrc" "$HOME/.bashrc"
 create_link "${PWD}/src/profile" "$HOME/.profile"
 create_link "${PWD}/src/bash_profile" "$HOME/.bash_profile"
-create_link "${PWD}/src/zshrc" "$HOME/.zshrc"
-create_link "${PWD}/src/zprofile" "$HOME/.zprofile"
+
+create_link "${PWD}/src/zshenv" "$HOME/.zshenv"
+mkdir -p ~/.config/zsh
+create_link "${PWD}/src/zsh/zshenv" "$HOME/.config/zsh/.zshenv"
+create_link "${PWD}/src/zsh/zshrc" "$HOME/.config/zsh/.zshrc"
+create_link "${PWD}/src/zsh/zprofile" "$HOME/.config/zsh/.zprofile"
 ###
 exit 0
