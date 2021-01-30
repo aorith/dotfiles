3e09bde4f69a1468d349f146339e4f1507a09d36
Manuel Sanchez Pinar <aomanu@gmail.com>
2021-01-30 10:48:49 +0100

    Add switch-theme to alacritty

---
+++ topics/alacritty/src/alacritty/bin/switch-theme.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+PATH="/usr/local/bin:/bin:/usr/bin"
+THEMEP="${HOME}/.config/alacritty/themes/alacritty-theme/themes"
+CONFIG="${HOME}/.config/alacritty/alacritty.yml"
+
+if [[ "$1" == "reset" ]]; then
+    ln -sf ~/.config/alacritty/themes/apprentice.yaml ~/.config/alacritty/theme.yml && \
+        cp "${CONFIG}" "/tmp/.alacritty.yml" && \
+        echo >> "${CONFIG}" && \
+        mv "/tmp/.alacritty.yml" "${CONFIG}"
+else
+    cd "${THEMEP}" || exit 1
+    find . -type f \( -name "*.yml" -o -name "*.yaml" \) -not -name "schemes.yaml" | fzf --preview "head -n 100 {} && ln -sf ${THEMEP}/$(basename {}) ${HOME}/.config/alacritty/theme.yml && cp ${CONFIG} /tmp/.alacritty.yml && echo >> ${CONFIG} && mv /tmp/.alacritty.yml ${CONFIG}"
+fi
