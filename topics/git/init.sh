62e8935322681f79e506363df464f353f44a99bd
Manuel Sanchez Pinar <aomanu@gmail.com>
2021-01-31 11:59:44 +0100

    Create a default symlink to private githome

--- topics/git/init.sh
+++ topics/git/init.sh
@@ -1,6 +1,7 @@
 # vim: ft=bash
 
 ### bootstrap
+create_link "${PRIVATE_GITHOME}" "${GITHOME}/00_SYNC"
 create_link "${PWD}/src/git" "$HOME/.config/git"
 [[ -f "$HOME/.gitconfig" ]] && rm "$HOME/.gitconfig" || true
 ###
