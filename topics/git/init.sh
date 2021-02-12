589aca38bad3e3c68dba02711b56259c6ce5736c
Manuel Sanchez Pinar <aomanu@gmail.com>
2021-02-12 21:05:59 +0100

    Only if path exists

--- topics/git/init.sh
+++ topics/git/init.sh
@@ -1,7 +1,7 @@
 # vim: ft=bash
 
 ### bootstrap
-create_link "${PRIVATE_GITHOME}" "${GITHOME}/00_SYNC"
+[[ -d $PRIVATE_GITHOME ]] && create_link "${PRIVATE_GITHOME}" "${GITHOME}/00_SYNC" || true
 create_link "${PWD}/src/git" "$HOME/.config/git"
 [[ -f "$HOME/.gitconfig" ]] && rm "$HOME/.gitconfig" || true
 ###
