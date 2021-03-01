f7b0e3892c40ba707b0cb7b98f5e9827d0ca19c6
Manuel Sanchez Pinar <aomanu@gmail.com>
2021-03-01 17:33:36 +0100

    Add xdg dirs

--- topics/xorg/init.sh
+++ topics/xorg/init.sh
@@ -3,12 +3,21 @@
 # Linux only
 [[ "$(uname -s)" != "Linux" ]] && exit 0
 
+_create_user_dirs() {
+    . "$PWD/src/config/user-dirs.dirs"
+    for d in $(awk -F'=' '{print $1}' < "${PWD}/src/config/user-dirs.dirs"); do
+        mkdir -p "${!d}"
+    done
+}
+
 ###
 case $HOSTNAME in
     x220)
         create_link "${PWD}/src/config/fontconfig" "$HOME/.config/fontconfig"
         create_link "${PWD}/src/config/Xresources" "$HOME/.config/Xresources"
         create_link "${PWD}/src/config/i3" "$HOME/.config/i3"
+        create_link "${PWD}/src/config/user-dirs.dirs" "$HOME/.config/user-dirs.dirs"
+        _create_user_dirs
         create_link "${PWD}/src/xinitrc" "$HOME/.xinitrc"
         sudo mkdir -p "/etc/X11/xorg.conf.d"
         # create_link "${PWD}/etc/X11/xorg.conf.d/20-intel.conf" "/etc/X11/xorg.conf.d/20-intel.conf"
@@ -18,4 +27,5 @@ case $HOSTNAME in
     *) ;;
 esac
 ###
+unset _create_user_dirs
 exit 0
