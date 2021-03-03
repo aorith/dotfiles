c8a77ac85b20168240ef8b7958bd7a6f81eaec42
Manuel Sanchez Pinar <aomanu@gmail.com>
2021-03-03 11:56:31 +0100

    Do not move to the center always

--- topics/xorg/src/config/i3/scripts/scratchpad-term.sh
+++ topics/xorg/src/config/i3/scripts/scratchpad-term.sh
@@ -1,10 +1,10 @@
-#!/usr/bin/env bash
+#!/bin/bash
 
-if ! xdotool search --classname XTermScratchpad &>/dev/null; then
-    xterm \
-        -bg '#212121' -fs 12 -name 'XTermScratchpad' \
+if ! xdotool search --classname 'scratchpad.tmux' &>/dev/null; then
+    xterm -bg '#212121' -fs 12 -name 'scratchpad.tmux' \
         -e 'tmux -u new-session -A -s "scratchpad"' &
     sleep 0.3
+    i3-msg '[instance="scratchpad.tmux"] scratchpad show; move position center' &>/dev/null
+else
+    i3-msg '[instance="scratchpad.tmux"] scratchpad show' &>/dev/null
 fi
-
-i3-msg '[instance="XTermScratchpad"] scratchpad show; move position center' &>/dev/null
