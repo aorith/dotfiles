73dfa501363d62c6ce6b83256315c0ca711bf974
Manuel Sanchez Pinar <aomanu@gmail.com>
2021-03-06 16:31:25 +0100

    git

--- topics/git/init.sh
+++ topics/git/init.sh
@@ -2,7 +2,8 @@
 
 ### bootstrap
 [[ -d $PRIVATE_GITHOME ]] && create_link "${PRIVATE_GITHOME}" "${GITHOME}/00_SYNC" || true
-create_link "${PWD}/src/git" "$HOME/.config/git"
+create_link "${PWD}/src/config/git" "$HOME/.config/git"
+create_link "${PWD}/src/config/tig" "$HOME/.config/tig"
 [[ -f "$HOME/.gitconfig" ]] && rm "$HOME/.gitconfig" || true
 ###
 exit 0
