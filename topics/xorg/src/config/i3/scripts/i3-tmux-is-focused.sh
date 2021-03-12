7e12c73a245ea9302d2ec520f1ed0e80e0fbc651
Manuel Sanchez Pinar <aomanu@gmail.com>
2021-03-12 07:00:31 +0100

    Move-out xorg topic

--- topics/xorg/src/config/i3/scripts/i3-tmux-is-focused.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-active_window=$(xdotool getactivewindow)
-active_window_pid=$(xdotool getwindowpid $active_window)
-current_window_cmd="$(ps -o cmd= -p $active_window_pid)"
-[[ ${current_window_cmd,,} =~ xterm.*tmux ]]
