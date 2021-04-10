38389033e00bffe8a203144731569b494e6f9e12
Manuel Sanchez Pinar <aomanu@gmail.com>
2021-04-10 16:39:16 +0200

    Msg format

--- utils/messages.sh
+++ utils/messages.sh
@@ -8,7 +8,7 @@ link_error()    { echo -e " $(tput setaf 1)          ✖$(tput sgr0)  $*"; }
 log_header()    { printf '\r\n \033[01;36m- %s\033[0m\n' "$*"; }
 log_info()      { printf '\r  [ \033[00;34mINFO\033[0m ] %s\n' "$*"; }
 log_success()   { printf '\r\033[2K  [ \033[00;32mOK\033[0m ] %s\n' "$*"; }
-log_warn()      { printf '\r\033[2K  [\033[0;33mWARN\033[0m] %s\n' "$*"; }
-log_error()     { printf '\r\033[2K  [\033[0;31mERROR\033[0m] %s\n' "$*"; }
+log_warn()      { printf '\r\033[2K  [ \033[0;33mWARN\033[0m ] %s\n' "$*"; }
+log_error()     { printf '\r\033[2K  [ \033[0;31mERROR\033[0m ] %s\n' "$*"; }
 
 set +a
