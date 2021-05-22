21e0fc82d7a6f20b0e7a10a19bb9b195bef07277
Manuel Sanchez Pinar <aomanu@gmail.com>
2021-05-22 21:36:39 +0200

    Update alacritty config

--- topics/alacritty/src/alacritty/bin/switch-theme.sh
+++ topics/alacritty/src/alacritty/bin/switch-theme.sh
@@ -10,5 +10,8 @@ if [[ "$1" == "reset" ]]; then
         mv "/tmp/.alacritty.yml" "${CONFIG}"
 else
     cd "${THEMEP}" || exit 1
-    find . -type f \( -name "*.yml" -o -name "*.yaml" \) -not -name "schemes.yaml" | fzf --preview "head -n 100 {} && ln -sf ${THEMEP}/$(basename {}) ${HOME}/.config/alacritty/theme.yml && cp ${CONFIG} /tmp/.alacritty.yml && echo >> ${CONFIG} && mv /tmp/.alacritty.yml ${CONFIG}"
+    {
+        find . -type f \( -name "*.yml" -o -name "*.yaml" \) -not -name "schemes.yaml";
+        find ../../ -type f -maxdepth 1 -mindepth 1 \( -name "*.yml" -o -name "*.yaml" \) -not -name "schemes.yaml"
+    } | fzf --preview "head -n 100 {} && ln -sf ${THEMEP}/$(basename {}) ${HOME}/.config/alacritty/theme.yml && cp ${CONFIG} /tmp/.alacritty.yml && echo >> ${CONFIG} && mv /tmp/.alacritty.yml ${CONFIG}"
 fi
