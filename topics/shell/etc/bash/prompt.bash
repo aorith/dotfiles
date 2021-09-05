1e2d078a4924ea92090b2cc7713c3620a365c4da
Manuel Sanchez Pinar <aomanu@gmail.com>
2021-09-05 20:50:41 +0200

    Normal Prompt

--- topics/shell/etc/bash/prompt.bash
+++ topics/shell/etc/bash/prompt.bash
@@ -80,7 +80,8 @@ __prompt_command () {
     [[ -z "$SSH_CLIENT" ]] || onssh="${my_ylw2}\h:${my_rst}"
     [[ -z "$IN_NIX_SHELL" ]] || onnixshell="${my_red2}(${name})${my_rst} "
 
-    PS1="\n\[\033]0;\u@\h:\w\007\]${tc}${ms}${my_rst} ${onssh}${wdc}\w${_ps1_git_branch}${my_rst}${_ps1_jobs}${ep} ${onnixshell}${my_blu}❯${my_rst} "
+    #PS1="\n\[\033]0;\u@\h:\w\007\]${tc}${ms}${my_rst} ${onssh}${wdc}\w${_ps1_git_branch}${my_rst}${_ps1_jobs}${ep} ${onnixshell}${my_blu}❯${my_rst} "
+    PS1="\n\[\033]0;\u@\h:\w\007\]${tc}${ms}${my_rst} ${onssh}${wdc}\w${_ps1_git_branch}${my_rst}${_ps1_jobs}${ep} ${onnixshell}${my_blu}\$${my_rst} "
 
     unset _ps1_start_timer
 }
