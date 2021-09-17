45d083d98605858366bb8d1b005058f2eb214136
Manuel Sanchez Pinar <aomanu@gmail.com>
2021-09-18 00:18:03 +0200

    Create n/vim directories with the bootstrap

--- topics/nvim/init.sh
+++ topics/nvim/init.sh
@@ -2,5 +2,8 @@
 
 ### bootstrap
 create_link "${PWD}/src/nvim" "$HOME/.config/nvim"
+mkdir -p ~/.local/share/nvim/swap
+mkdir -p ~/.local/share/nvim/undodir
+mkdir -p ~/.local/share/nvim/backup
 ###
 exit 0
