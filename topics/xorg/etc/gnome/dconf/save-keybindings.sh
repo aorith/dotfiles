0ffc5812a5ad79ada48e9abe0af5c411c2c2a32f
Manuel Sanchez Pinar <aomanu@gmail.com>
2021-09-17 05:19:02 +0200

    Deprecate xorg

--- topics/xorg/etc/gnome/dconf/save-keybindings.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-
-dconf dump /org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/ > custom-keybindings.conf
-dconf dump /org/gnome/desktop/wm/keybindings/ > gnome-keybindings.conf
-
