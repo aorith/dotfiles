b847aae193cbc0a9f3a1cbd1b124859d5a0051fa
Manuel Sanchez Pinar <aomanu@gmail.com>
2021-09-19 11:57:08 +0200

    less: colors

--- topics/shell/etc/bash/constants.bash
+++ topics/shell/etc/bash/constants.bash
@@ -17,13 +17,13 @@ elif [[ -r "/usr/local/bin/lesspipe.sh" ]]; then
     export LESSOPEN="|/usr/local/bin/lesspipe.sh %s" LESS_ADVANCED_PREPROCESSOR=1
 fi
 
-export LESS_TERMCAP_mb=$'\033[01;31m'      # begins blinking
-export LESS_TERMCAP_md=$'\033[01;38;5;74m' # begins bold
-export LESS_TERMCAP_me=$'\033[0m'          # ends mode
-export LESS_TERMCAP_so=$'\033[30;43m'      # begins standout-mode
-export LESS_TERMCAP_se=$'\033[0m'          # ends standout-mode
-export LESS_TERMCAP_us=$'\033[1;32m'       # begins underline
-export LESS_TERMCAP_ue=$'\033[0m'          # ends underline
+#export LESS_TERMCAP_mb=$'\033[01;31m'            # begins blinking
+export LESS_TERMCAP_md=$(tput setaf 4; tput bold) # begins bold (headers, titles ...)
+#export LESS_TERMCAP_me=$'\033[0m'                # ends mode
+export LESS_TERMCAP_so=$(tput rev)                # begins standout-mode (highligh and statusbar)
+#export LESS_TERMCAP_se=$'\033[0m'                # ends standout-mode
+export LESS_TERMCAP_us=$(tput setaf 1)            # begins underline (key words)
+#export LESS_TERMCAP_ue=$'\033[0m'                # ends underline
 
 export XDG_CONFIG_HOME="$HOME/.config"
 export XDG_DATA_HOME="$HOME/.local/share"
