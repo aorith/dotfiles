3ad6f36de0a8d04643d9d51b7608ccddd666195a
Manuel Sanchez Pinar <aomanu@gmail.com>
2021-09-26 20:34:28 +0200

    bash: simplify settings

--- topics/shell/etc/bash/settings.bash
+++ topics/shell/etc/bash/settings.bash
@@ -1,21 +1,9 @@
 # vim: ft=bash
 # Sourced from: bash_profile
 
-shopt -s checkwinsize
-shopt -s histappend
-shopt -s cmdhist # Save multi-line commands as one command
-
 complete -cf sudo
 
-if ((BASH_VERSINFO[0] >= 5)); then
-    # History
-    # Enable history expansion with space
-    # E.g. typing !!<space> will replace the !! with your last command
-    bind Space:magic-space
-
-    shopt -s globstar 2> /dev/null # (enables ** to recurse all directories)
-    shopt -s autocd 2> /dev/null # Prepend cd to directory names automatically
-fi
+shopt -s checkwinsize histappend cmdhist globstar autocd 2>/dev/null
 
 export HISTFILE="${XDG_DATA_HOME:-$HOME/.local/share}/.bash_history"
 export HISTCONTROL="erasedups:ignoreboth"
