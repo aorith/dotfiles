d4ab2443497b8638c34986f0dfcba32c7fabb22a
Manuel Sanchez Pinar <aomanu@gmail.com>
2022-02-22 08:51:22 +0100

    alacritty

--- topics/alacritty/init.sh
+++ topics/alacritty/init.sh
@@ -4,21 +4,5 @@
 [[ -d "$HOME/.config/alacritty" ]] || mkdir -p "$HOME/.config/alacritty"
 create_link "${PWD}/src/alacritty" "$HOME/.config/alacritty"
 create_link "${PWD}/src/alacritty/$(uname -s).yml" "${PWD}/src/alacritty/env.yml"
-
-# macos only
-[[ "$(uname -s)" == "Darwin" ]] || exit $_SKIP
-
-if ! infocmp alacritty &>/dev/null; then
-    [[ -n "$SSH_CLIENT" ]] && { log_warn "Not bootstrapping terminfo over ssh"; exit 0; }
-    current_user=$(whoami)
-    sudo tic -xe alacritty,alacritty-direct etc/alacritty.info
-    [[ -d "/root" ]] && sudo cp -r ~/.terminfo /root/
-    [[ -d "/var/root" ]] && sudo cp -r ~/.terminfo /var/root/
-    sudo mkdir -p /usr/local/share/man/man1
-    sudo cp etc/alacritty.1.gz /usr/local/share/man/man1/
-    sudo chown -R ${current_user}: ~/.terminfo
-    sudo chown -R ${current_user}: /usr/local/share/man/man1/alacritty.1.gz
-    unset version current_user
-fi
 ###
 exit 0
