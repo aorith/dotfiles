3d9c87bd76e8b7bfb234a56742c4bae4913d23cf
Manuel Sanchez Pinar <aomanu@gmail.com>
2022-04-30 12:07:39 +0200

    fix: arg order

--- topics/alacritty/src/alacritty/bin/switch-theme.sh
+++ topics/alacritty/src/alacritty/bin/switch-theme.sh
@@ -13,6 +13,6 @@ else
     {
         find mine -type f \( -name "*.yml" -o -name "*.yaml" \) -not -name "schemes.yaml";
         find alacritty-theme/themes -type f \( -name "*.yml" -o -name "*.yaml" \) -not -name "schemes.yaml";
-        find tempus-themes-alacritty -type f -maxdepth 1 -mindepth 1 \( -name "*.yml" -o -name "*.yaml" \) -not -name "schemes.yaml"
+        find tempus-themes-alacritty -maxdepth 1 -mindepth 1 -type f \( -name "*.yml" -o -name "*.yaml" \) -not -name "schemes.yaml"
     } | fzf --preview "head -n 100 {} && ln -sf ${BASET}/$(basename {}) ${HOME}/.config/alacritty/theme.yml && cp ${CONFIG} /tmp/.alacritty.yml && echo >> ${CONFIG} && mv /tmp/.alacritty.yml ${CONFIG}"
 fi
