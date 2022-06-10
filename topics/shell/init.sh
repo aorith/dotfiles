eeee84f6a1279629c09b3bf591ee315cce672628
Manuel Sanchez Pinar <aomanu@gmail.com>
2022-06-10 13:23:08 +0200

    shell: clean

--- topics/shell/init.sh
+++ topics/shell/init.sh
@@ -1,7 +1,6 @@
 # vim: ft=bash
 
 ### bootstrap
-create_link "${PWD}/src/bash/xprofile" "$HOME/.xprofile"
 create_link "${PWD}/src/bash/inputrc" "$HOME/.inputrc"
 create_link "${PWD}/src/bash/bashrc" "$HOME/.bashrc"
 create_link "${PWD}/src/bash/profile" "$HOME/.profile"
