7dabc202e90c84703b246eb2c74a86671a143fb8
Manuel Sanchez Pinar <aomanu@gmail.com>
2022-06-11 17:51:55 +0200

    upd

--- topics/shell/etc/bash/settings.bash
+++ topics/shell/etc/bash/settings.bash
@@ -4,6 +4,7 @@
 complete -cf sudo
 
 shopt -s checkwinsize histappend cmdhist globstar autocd 2>/dev/null
+set -o notify # notify completed background jobs inmediately
 
 export HISTFILE="${XDG_DATA_HOME:-$HOME/.local/share}/.bash_history"
 export HISTCONTROL="erasedups:ignoreboth"
