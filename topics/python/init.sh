226e61e5fd5dd3fafb499de669a694eeb201d1a4
Manuel Sanchez Pinar <aomanu@gmail.com>
2022-07-31 08:53:32 +0200

    python: use git by default

--- topics/python/init.sh
+++ topics/python/init.sh
@@ -1,30 +1,32 @@
 # vim: ft=bash
 
+[[ -d "$PYENV_ROOT" ]] || { echo "Missing 'PYENV_ROOT' variable."; exit 1; }
+
 _debian_deps() {
     sudo apt update; sudo apt install make build-essential libssl-dev zlib1g-dev \
         libbz2-dev libreadline-dev libsqlite3-dev wget curl llvm \
         libncursesw5-dev xz-utils tk-dev libxml2-dev libxmlsec1-dev libffi-dev liblzma-dev
 }
 
-case $(hostname) in
+_clone_update_pyenv() {
+    if [[ ! -e "$PYENV_ROOT" ]]; then
+        mkdir -p "$(dirname -- "$PYENV_ROOT")"
+        git clone --depth 1 https://github.com/pyenv/pyenv.git "$PYENV_ROOT" >/dev/null && \
+            log_success "Cloned pyenv"
+    else
+        cd "$PYENV_ROOT" && git pull >/dev/null && log_success "Updated pyenv"
+    fi
+}
+
+case $HOSTNAME in
     moria*)
-        brew install --formulae "pyenv"
+        _clone_update_pyenv
         ;;
     debian)
         _debian_deps
         ;;
     *) exit "$_SKIP" ;;
 esac
-
-[[ -n "$PYENV_ROOT" ]] || exit 1
-
-if [[ ! -e "$PYENV_ROOT" ]]; then
-    mkdir -p "$(dirname -- "$PYENV_ROOT")"
-    git clone --depth 1 https://github.com/pyenv/pyenv.git "$PYENV_ROOT"
-fi
-if [[ ! -e "$PYENV_ROOT"/plugins/pyenv-virtualenv ]]; then
-    git clone --depth 1 https://github.com/pyenv/pyenv-virtualenv.git "$PYENV_ROOT"/plugins/pyenv-virtualenv
-fi
 
 ### bootstrap
 version="3.10.4"
@@ -33,8 +35,11 @@ version="3.10.4"
 # pyenv global 3.8.6  # make it global
 # python3 --version  # verify
 
-pyenv install "$version"
-pyenv global "$version"
+if [[ "$(pyenv version | awk '{print $1}')" != "$version" ]]; then
+    pyenv install "$version"
+    pyenv global "$version"
+fi
+log_success "Using version $version"
 unset version
 ###
 exit 0
