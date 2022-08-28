b54ff5a5cb4e1e2323a44b2c52761b816b97886b
Manuel Sanchez Pinar <aomanu@gmail.com>
2022-08-28 09:38:01 +0200

    alacritty

--- topics/alacritty/src/alacritty/bin/switch-theme.sh
+++ topics/alacritty/src/alacritty/bin/switch-theme.sh
@@ -1,18 +1,13 @@
 #!/usr/bin/env bash
+set -e
 
 BASET="${HOME}/.config/alacritty/themes"
 CONFIG="${HOME}/.config/alacritty/alacritty.yml"
 
-if [[ "$1" == "reset" ]]; then
-    ln -sf ~/.config/alacritty/themes/mine/apprentice.yaml ~/.config/alacritty/theme.yml && \
-        cp "${CONFIG}" "/tmp/.alacritty.yml" && \
-        printf '\n\n' >> "${CONFIG}" && \
-        mv "/tmp/.alacritty.yml" "${CONFIG}"
-else
-    cd "$BASET" || exit 1
-    {
-        find mine -type f \( -name "*.yml" -o -name "*.yaml" \) -not -name "schemes.yaml";
-        #find alacritty-theme/themes -type f \( -name "*.yml" -o -name "*.yaml" \) -not -name "schemes.yaml";
-        #find tempus-themes-alacritty -maxdepth 1 -mindepth 1 -type f \( -name "*.yml" -o -name "*.yaml" \) -not -name "schemes.yaml";
-    } | fzf --preview "head -n 100 {} && ln -sf ${BASET}/$(basename {}) ${HOME}/.config/alacritty/theme.yml && cp ${CONFIG} /tmp/.alacritty.yml && echo >> ${CONFIG} && mv /tmp/.alacritty.yml ${CONFIG}"
-fi
+cd "$BASET" || exit 1
+
+{
+    find mine -type f \( -name "*.yml" -o -name "*.yaml" \) -not -name "schemes.yaml";
+    #find alacritty-theme/themes -type f \( -name "*.yml" -o -name "*.yaml" \) -not -name "schemes.yaml";
+    #find tempus-themes-alacritty -maxdepth 1 -mindepth 1 -type f \( -name "*.yml" -o -name "*.yaml" \) -not -name "schemes.yaml";
+} | fzf --preview "head -n 100 {} && ln -sf ${BASET}/$(basename {}) ${HOME}/.config/alacritty/theme.yml && cp ${CONFIG} /tmp/.alacritty.yml && echo >> ${CONFIG} && mv /tmp/.alacritty.yml ${CONFIG}"
