bae2039836f4382ae2c3003003a26ec2b96c66e2
Manuel Sanchez Pinar <aomanu@gmail.com>
2022-08-28 09:14:17 +0200

    alacritty: theme script

--- topics/alacritty/src/alacritty/bin/switch-theme.sh
+++ topics/alacritty/src/alacritty/bin/switch-theme.sh
@@ -12,7 +12,7 @@ else
     cd "$BASET" || exit 1
     {
         find mine -type f \( -name "*.yml" -o -name "*.yaml" \) -not -name "schemes.yaml";
-        find alacritty-theme/themes -type f \( -name "*.yml" -o -name "*.yaml" \) -not -name "schemes.yaml";
-        find tempus-themes-alacritty -maxdepth 1 -mindepth 1 -type f \( -name "*.yml" -o -name "*.yaml" \) -not -name "schemes.yaml"
+        #find alacritty-theme/themes -type f \( -name "*.yml" -o -name "*.yaml" \) -not -name "schemes.yaml";
+        #find tempus-themes-alacritty -maxdepth 1 -mindepth 1 -type f \( -name "*.yml" -o -name "*.yaml" \) -not -name "schemes.yaml";
     } | fzf --preview "head -n 100 {} && ln -sf ${BASET}/$(basename {}) ${HOME}/.config/alacritty/theme.yml && cp ${CONFIG} /tmp/.alacritty.yml && echo >> ${CONFIG} && mv /tmp/.alacritty.yml ${CONFIG}"
 fi
