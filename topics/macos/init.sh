0163e34016cfbdf25158dd45e5468868bfc4f273
Manuel Sanchez Pinar <aomanu@gmail.com>
2022-08-31 20:56:08 +0200

    fix: alacritty option+space

--- topics/macos/init.sh
+++ topics/macos/init.sh
@@ -4,5 +4,6 @@
 [[ "$(uname -s)" == "Darwin" ]] || exit "$_SKIP"
 
 # Fix Option+Space inserting non-breaking space (0xA0) " "
+# UPDATE: doesn't work reliably, fixed directly in alacritty
 mkdir -p ~/Library/KeyBindings
 create_link "${PWD}/src/DefaultKeyBinding.dict" "$HOME/Library/KeyBindings/DefaultKeyBinding.dict"
