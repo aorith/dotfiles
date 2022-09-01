f74c37e852ec4e6c50c5fa190ba1b316428dfa63
Manuel Sanchez Pinar <aomanu@gmail.com>
2022-09-01 09:04:59 +0200

    macos: key repeat config

--- topics/macos/init.sh
+++ topics/macos/init.sh
@@ -7,3 +7,7 @@
 # UPDATE: doesn't work reliably, fixed directly in alacritty
 mkdir -p ~/Library/KeyBindings
 create_link "${PWD}/src/DefaultKeyBinding.dict" "$HOME/Library/KeyBindings/DefaultKeyBinding.dict"
+
+# KeyRepeat
+defaults write -g InitialKeyRepeat -int 13
+defaults write -g KeyRepeat -int 2
