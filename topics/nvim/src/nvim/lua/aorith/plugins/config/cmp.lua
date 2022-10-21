7a57c9cb6ef57d6182deee10ef0afa9a17a3e34f
Manuel Sanchez Pinar <aomanu@gmail.com>
2022-10-21 21:02:35 +0200

    nvim: update

--- topics/nvim/src/nvim/lua/aorith/plugins/config/cmp.lua
+++ topics/nvim/src/nvim/lua/aorith/plugins/config/cmp.lua
@@ -1,71 +1,62 @@
-local cmp = require'cmp'
+local cmp = require 'cmp'
 
 cmp.setup({
-  snippet = {
-    -- REQUIRED - you must specify a snippet engine
-    expand = function(args)
-      vim.fn["vsnip#anonymous"](args.body) -- For `vsnip` users.
-      -- require('luasnip').lsp_expand(args.body) -- For `luasnip` users.
-      -- require('snippy').expand_snippet(args.body) -- For `snippy` users.
-      -- vim.fn["UltiSnips#Anon"](args.body) -- For `ultisnips` users.
-    end,
-  },
-  window = {
-    -- completion = cmp.config.window.bordered(),
-    -- documentation = cmp.config.window.bordered(),
-  },
-  mapping = cmp.mapping.preset.insert({
-      ['<C-p>'] = cmp.mapping.select_prev_item(),
-      ['<C-n>'] = cmp.mapping.select_next_item(),
-      ['<C-b>'] = cmp.mapping.scroll_docs(-4),
-      ['<C-f>'] = cmp.mapping.scroll_docs(4),
-      ['<C-Space>'] = cmp.mapping.complete(),
-      ['<C-e>'] = cmp.mapping.abort(),
-      ['<CR>'] = cmp.mapping.confirm({ select = true }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
-      ["<Tab>"] = cmp.mapping(function(fallback)
-          if cmp.visible() then
-              cmp.select_next_item()
-          else
-              fallback()
-          end
-      end, {"i", "s"}),
-  }),
-  sources = cmp.config.sources({
-    { name = 'nvim_lsp' },
-    { name = 'vsnip' }, -- For vsnip users.
-    -- { name = 'luasnip' }, -- For luasnip users.
-    -- { name = 'ultisnips' }, -- For ultisnips users.
-    -- { name = 'snippy' }, -- For snippy users.
-  }, {
-    { name = 'buffer' },
-  })
+    window = {
+        completion = cmp.config.window.bordered(),
+        documentation = cmp.config.window.bordered(),
+    },
+    mapping = cmp.mapping.preset.insert({
+        ['<C-p>'] = cmp.mapping.select_prev_item(),
+        ['<C-n>'] = cmp.mapping.select_next_item(),
+        ['<C-b>'] = cmp.mapping.scroll_docs(-4),
+        ['<C-f>'] = cmp.mapping.scroll_docs(4),
+        ['<C-Space>'] = cmp.mapping.complete(),
+        ['<C-e>'] = cmp.mapping.abort(),
+        ['<CR>'] = cmp.mapping.confirm({ select = true }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
+        ["<Tab>"] = cmp.mapping(function(fallback)
+            if cmp.visible() then
+                cmp.select_next_item()
+            else
+                fallback()
+            end
+        end, { "i", "s" }),
+    }),
+    sources = cmp.config.sources({
+        { name = 'nvim_lsp' },
+        -- { name = 'vsnip' }, -- For vsnip users.
+        -- { name = 'luasnip' }, -- For luasnip users.
+        -- { name = 'ultisnips' }, -- For ultisnips users.
+        -- { name = 'snippy' }, -- For snippy users.
+    }, {
+        { name = 'buffer' },
+    })
 })
 
 -- Set configuration for specific filetype.
 cmp.setup.filetype('gitcommit', {
-  sources = cmp.config.sources({
-    { name = 'cmp_git' }, -- You can specify the `cmp_git` source if you were installed it.
-  }, {
-    { name = 'buffer' },
-  })
+    sources = cmp.config.sources({
+        { name = 'cmp_git' }, -- You can specify the `cmp_git` source if you were installed it.
+    }, {
+        { name = 'buffer' },
+    })
 })
 
 -- Use buffer source for `/` and `?` (if you enabled `native_menu`, this won't work anymore).
 cmp.setup.cmdline({ '/', '?' }, {
-  mapping = cmp.mapping.preset.cmdline(),
-  sources = {
-    { name = 'buffer' }
-  }
+    mapping = cmp.mapping.preset.cmdline(),
+    sources = {
+        { name = 'buffer' }
+    }
 })
 
 -- Use cmdline & path source for ':' (if you enabled `native_menu`, this won't work anymore).
 cmp.setup.cmdline(':', {
-  mapping = cmp.mapping.preset.cmdline(),
-  sources = cmp.config.sources({
-    { name = 'path' }
-  }, {
-    { name = 'cmdline' }
-  })
+    mapping = cmp.mapping.preset.cmdline(),
+    sources = cmp.config.sources({
+        { name = 'path' }
+    }, {
+        { name = 'cmdline' }
+    })
 })
 
 -- Set up lspconfig.
@@ -73,37 +64,38 @@ local capabilities = require('cmp_nvim_lsp').default_capabilities(vim.lsp.protoc
 -- Replace <YOUR_LSP_SERVER> with each lsp server you've enabled.
 
 
-local flake_ignores = {"E203", -- whitespace before :
-                       "W503", -- line break before binary operator
-                       "E501"} -- line too long
+local flake_ignores = { "E203", -- whitespace before :
+    "W503", -- line break before binary operator
+    "E501" } -- line too long
 
-require'lspconfig'.pylsp.setup {
-  capabilities=capabilities,
-  filetypes = {'python'},
-  settings = {
-    formatCommand = {"black"},
-    pylsp = {
-      plugins = {
-        -- jedi_completion = {fuzzy = true},
-        -- jedi_completion = {eager=true},
-        jedi_completion = {
-          include_params = true,
-        },
-        flake8 = {
-          enabled=  true,
-          ignore = table.concat(flake_ignores, ",")
-        },
-        jedi_signature_help = {enabled = true},
-        jedi = {},
-        pyflakes={enabled=true},
-        pylint = {args = {'--ignore=E501,E231', '-'}, enabled=true, debounce=200},
-        pylsp_mypy={enabled=false},
-        pycodestyle={
-          enabled=true,
-          ignore={'E501', 'E231'},
-        maxLineLength=120},
-        yapf={enabled=true}
-      }
+require 'lspconfig'.pylsp.setup {
+    capabilities = capabilities,
+    filetypes = { 'python' },
+    settings = {
+        formatCommand = { "black" },
+        pylsp = {
+            plugins = {
+                -- jedi_completion = {fuzzy = true},
+                -- jedi_completion = {eager=true},
+                jedi_completion = {
+                    include_params = true,
+                },
+                flake8 = {
+                    enabled = true,
+                    ignore = table.concat(flake_ignores, ",")
+                },
+                jedi_signature_help = { enabled = true },
+                jedi = {},
+                pyflakes = { enabled = true },
+                pylint = { args = { '--ignore=E501,E231', '-' }, enabled = true, debounce = 200 },
+                pylsp_mypy = { enabled = false },
+                pycodestyle = {
+                    enabled = true,
+                    ignore = { 'E501', 'E231' },
+                    maxLineLength = 120
+                },
+                yapf = { enabled = true }
+            }
+        }
     }
-  }
 }
