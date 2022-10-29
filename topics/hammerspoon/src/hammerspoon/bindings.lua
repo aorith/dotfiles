e70294890c8abf2b5aeadb636f22fde2ed8363d6
Manuel Sanchez Pinar <aomanu@gmail.com>
2022-10-29 16:19:31 +0200

    hammerspoon: switch to wezterm

--- topics/hammerspoon/src/hammerspoon/bindings.lua
+++ topics/hammerspoon/src/hammerspoon/bindings.lua
@@ -1,16 +1,15 @@
 -- Toggle Alacritty
-hs.hotkey.bind({"cmd"}, "º", function()
-    local name = "alacritty"
-    --local name = "wezterm"
-    local app = hs.application.find(name)
-    if app then
-        if app:isFrontmost() then
-            app:hide()
-        else
-            app:activate()
-        end
+hs.hotkey.bind({ "cmd" }, "º", function()
+  --local name = "alacritty"
+  local name = "wezterm"
+  local app = hs.application.find(name)
+  if app then
+    if app:isFrontmost() then
+      app:hide()
     else
-        hs.application.launchOrFocus(name)
+      app:activate()
     end
+  else
+    hs.application.launchOrFocus(name)
+  end
 end)
-
