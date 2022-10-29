260ef021d263a7f189ff75b0e031ff3e9fad69a9
Manuel Sanchez Pinar <aomanu@gmail.com>
2022-10-29 14:23:45 +0200

    wezterm: update

--- topics/wezterm/src/wezterm/bin/rename_wezterm_title.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash -l
-
-export PATH="/opt/homebrew/opt/coreutils/libexec/gnubin:${PATH}"
-printf 'Enter new title: '
-read title || exit 0
-[[ -n "$title" ]] || exit 0
-echo -e "\x1b]1337;SetUserVar=panetitle=$(echo -n "$title" | base64)\x07" || exit 1
