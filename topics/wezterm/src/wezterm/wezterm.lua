c772a9d3439ebc9158abd5371a0103201988d674
Manuel Sanchez Pinar <aomanu@gmail.com>
2022-10-31 16:05:29 +0100

    wezterm: update

--- topics/wezterm/src/wezterm/wezterm.lua
+++ topics/wezterm/src/wezterm/wezterm.lua
@@ -40,9 +40,52 @@ config.keys = {
   { key = "c", mods = "CMD", action = wezterm.action.CopyTo("ClipboardAndPrimarySelection") },
 }
 
+-- TODO: implement this with wezterm mux
+local macos_tmux_keys = {
+  { key = "w", mods = "CMD", action = wezterm.action.SendString("\x1bw") }, -- close pane/window
+  { key = "t", mods = "CMD", action = wezterm.action.SendString("\x1bt") }, -- new pane
+  { key = "T", mods = "CMD|SHIFT", action = wezterm.action.SendString("\x1bT") }, -- new session
+  { key = "h", mods = "CMD", action = wezterm.action.SendString("\x1bh") }, -- switch pane
+  { key = "j", mods = "CMD", action = wezterm.action.SendString("\x1bj") }, -- switch pane
+  { key = "k", mods = "CMD", action = wezterm.action.SendString("\x1bk") }, -- switch pane
+  { key = "l", mods = "CMD", action = wezterm.action.SendString("\x1bl") }, -- switch pane
+  { key = "H", mods = "CMD|SHIFT", action = wezterm.action.SendString("\x1bH") }, -- move pane
+  { key = "J", mods = "CMD|SHIFT", action = wezterm.action.SendString("\x1bJ") }, -- move pane
+  { key = "K", mods = "CMD|SHIFT", action = wezterm.action.SendString("\x1bK") }, -- move pane
+  { key = "L", mods = "CMD|SHIFT", action = wezterm.action.SendString("\x1bL") }, -- move pane
+  { key = "Return", mods = "CMD|SHIFT", action = wezterm.action.SendString("\x1b\r") }, -- switch layout
+  { key = "d", mods = "CMD", action = wezterm.action.SendString("\x1bd") }, -- new horiz pane
+  { key = "D", mods = "CMD|SHIFT", action = wezterm.action.SendString("\x1bD") }, -- new vert pane
+  { key = "s", mods = "CMD", action = wezterm.action.SendString("\x1bs") }, -- toggle synchronize panes
+  { key = "1", mods = "CMD", action = wezterm.action.SendString("\x1b1") }, -- switch to pane 1
+  { key = "2", mods = "CMD", action = wezterm.action.SendString("\x1b2") },
+  { key = "3", mods = "CMD", action = wezterm.action.SendString("\x1b3") },
+  { key = "4", mods = "CMD", action = wezterm.action.SendString("\x1b4") },
+  { key = "5", mods = "CMD", action = wezterm.action.SendString("\x1b5") },
+  { key = "6", mods = "CMD", action = wezterm.action.SendString("\x1b6") },
+  { key = "7", mods = "CMD", action = wezterm.action.SendString("\x1b7") },
+  { key = "8", mods = "CMD", action = wezterm.action.SendString("\x1b8") },
+  { key = "9", mods = "CMD", action = wezterm.action.SendString("\x1b9") },
+  { key = "g", mods = "CMD", action = wezterm.action.SendString("\x1bg") }, -- enter copy mode
+  { key = "r", mods = "CMD", action = wezterm.action.SendString("\x1br") }, -- rename pane
+  { key = "R", mods = "CMD|SHIFT", action = wezterm.action.SendString("\x1bR") }, -- rename session
+  { key = "Comma", mods = "CMD", action = wezterm.action.SendString("\x1b,") }, -- cycle sessions
+  { key = "n", mods = "CMD", action = wezterm.action.SendString("\x1bn") }, -- cycle panes
+  { key = "z", mods = "CMD", action = wezterm.action.SendString("\x1bz") }, -- toggle zoom (maximize pane)
+  { key = "f", mods = "CMD", action = wezterm.action.SendString("\x1bf") }, -- search
+  { key = "u", mods = "CMD", action = wezterm.action.SendString("\x1bu") }, -- tmux open url
+  { key = "i", mods = "CMD", action = wezterm.action.SendString("\x1bi") }, -- tmux server for
+  { key = "p", mods = "CMD", action = wezterm.action.SendString("\x1bm") }, -- tmux & bitwarden
+}
+
 if string.match(wezterm.target_triple, "darwin") then
+  -- they're in reverse between the built-in keyboard and the external MXKeys
   config.send_composed_key_when_left_alt_is_pressed = true
-  config.send_composed_key_when_right_alt_is_pressed = false
+  config.send_composed_key_when_right_alt_is_pressed = true
+
+  for i, v in ipairs(macos_tmux_keys) do
+    table.insert(config.keys, v)
+  end
 end
 
 return config
