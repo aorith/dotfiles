911612d66314bbb2008e31746e74742b93a6fb9a
Manuel Sanchez Pinar <aomanu@gmail.com>
2022-11-01 09:59:48 +0100

    wezterm: update

--- topics/wezterm/src/wezterm/wezterm.lua
+++ topics/wezterm/src/wezterm/wezterm.lua
@@ -2,15 +2,18 @@ local wezterm = require("wezterm")
 
 local config = {
   window_decorations = "RESIZE", -- RESIZE: don't show titlebar but allow resize
+  cursor_blink_rate = 0, -- disable blinking
   color_scheme = "kanagawabones",
   colors = {
     cursor_bg = "#52ad70",
     cursor_fg = "black",
     cursor_border = "#52ad70",
   },
+
   font_size = 16.0,
-  --font = wezterm.font("MesloLGMDZ Nerd Font Mono"),
+  font = wezterm.font("MesloLGMDZ Nerd Font Mono"),
   harfbuzz_features = { "calt=0", "clig=0", "liga=0" }, -- --> disable ligadures
+
   hide_tab_bar_if_only_one_tab = true,
   adjust_window_size_when_changing_font_size = false,
   disable_default_key_bindings = true,
