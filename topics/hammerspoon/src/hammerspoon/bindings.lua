7992e96dd44e84fe665738bc6a2a26aac2d717e5
Manuel Sanchez Pinar <aomanu@gmail.com>
2022-11-19 15:31:37 +0100

    hammerspoon: update

--- topics/hammerspoon/src/hammerspoon/bindings.lua
+++ topics/hammerspoon/src/hammerspoon/bindings.lua
@@ -1,15 +1,17 @@
 -- Toggle Alacritty
-hs.hotkey.bind({ "cmd" }, "º", function()
-  --local name = "alacritty"
-  local name = "wezterm"
-  local app = hs.application.find(name)
-  if app then
-    if app:isFrontmost() then
-      app:hide()
-    else
-      app:activate()
-    end
-  else
-    hs.application.launchOrFocus(name)
-  end
-end)
+local trigger_term = function()
+	--local name = "alacritty"
+	local name = "wezterm"
+	local app = hs.application.find(name)
+	if app then
+		if app:isFrontmost() then
+			app:hide()
+		else
+			app:activate()
+		end
+	else
+		hs.application.launchOrFocus(name)
+	end
+end
+
+hs.hotkey.bind({ "cmd" }, "º", trigger_term)
