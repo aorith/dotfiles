ffb1f0ba2a5f136480adc7e091743aa2236add14
Manuel Sanchez Pinar <aomanu@gmail.com>
2023-02-04 13:18:15 +0100

    neovim: move custom function

--- topics/nvim/src/nvim/lua/plugins/config/lsp/handlers.lua
+++ topics/nvim/src/nvim/lua/plugins/config/lsp/handlers.lua
@@ -77,7 +77,32 @@ local lsp_highlight_document = function(bufnr)
   })
 end
 
+M.custom_server_capabilities = function()
+  local active_clients = vim.lsp.get_active_clients()
+  local active_client_map = {}
+
+  for index, value in ipairs(active_clients) do
+    active_client_map[value.name] = index
+  end
+
+  vim.ui.select(vim.tbl_keys(active_client_map), {
+    prompt = "Select client:",
+    format_item = function(item)
+      return "capabilites for: " .. item
+    end,
+  }, function(choice)
+    print(
+      vim.inspect(vim.lsp.get_active_clients()[active_client_map[choice]].server_capabilities.executeCommandProvider)
+    )
+    vim.pretty_print(vim.lsp.get_active_clients()[active_client_map[choice]].server_capabilities)
+  end)
+end
+
 M.on_attach = function(client, bufnr)
+  vim.cmd(
+    "command! CheckLspServerCapabilities :lua =require('plugins.config.lsp.handlers').custom_server_capabilities()"
+  )
+
   -- disable formatting to null-ls takes care
   if client.name == "sumneko_lua" then
     client.server_capabilities.documentFormattingProvider = false
