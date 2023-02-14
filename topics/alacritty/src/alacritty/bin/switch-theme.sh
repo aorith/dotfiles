5c58891943a640ae7eb878baddb4bc0605b2d5f5
Manuel Sanchez Pinar <aomanu@gmail.com>
2023-02-14 21:14:10 +0100

    alacritty: update

--- topics/alacritty/src/alacritty/bin/switch-theme.sh
+++ topics/alacritty/src/alacritty/bin/switch-theme.sh
@@ -7,7 +7,6 @@ CONFIG="${HOME}/.config/alacritty/alacritty.yml"
 cd "$BASET" || exit 1
 
 {
-    find mine -type f \( -name "*.yml" -o -name "*.yaml" \) -not -name "schemes.yaml";
-    #find alacritty-theme/themes -type f \( -name "*.yml" -o -name "*.yaml" \) -not -name "schemes.yaml";
-    #find tempus-themes-alacritty -maxdepth 1 -mindepth 1 -type f \( -name "*.yml" -o -name "*.yaml" \) -not -name "schemes.yaml";
+    find mine/ -type f \( -name "*.yml" -o -name "*.yaml" \) -not -name "schemes.yaml";
+    find zenbones/ -type f \( -name "*.yml" -o -name "*.yaml" \) -not -name "schemes.yaml";
 } | fzf --preview "head -n 100 {} && ln -sf ${BASET}/$(basename {}) ${HOME}/.config/alacritty/theme.yml && cp ${CONFIG} /tmp/.alacritty.yml && echo >> ${CONFIG} && mv /tmp/.alacritty.yml ${CONFIG}"
