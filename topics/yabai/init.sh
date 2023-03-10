74f28df1019085aabf5964d323b5d9f050604bb4
Manuel Sanchez Pinar <aomanu@gmail.com>
2023-03-10 17:43:05 +0100

    cleanup

--- topics/yabai/init.sh
+++
@@ -1,7 +0,0 @@
-# vim: ft=bash
-
-# macos only
-[[ "$(uname -s)" == "Darwin" ]] || exit "$_SKIP"
-
-create_link "${PWD}/src/yabai" "$HOME/.config/yabai"
-create_link "${PWD}/src/skhd" "$HOME/.config/skhd"
