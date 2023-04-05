d07adeff9bfc6d12d99d0c0e115daf8fb68c65be
Manuel Sanchez Pinar <aomanu@gmail.com>
2023-04-05 18:55:18 +0200

    Remove nvim config, using a flake now:
        https://github.com/aorith/neovim-flake

--- topics/nvim/init.sh
+++
@@ -1,9 +0,0 @@
-# vim: ft=bash
-
-### bootstrap
-create_link "${PWD}/src/nvim" "$HOME/.config/nvim"
-mkdir -p ~/.local/share/nvim/swap
-mkdir -p ~/.local/share/nvim/undodir
-mkdir -p ~/.local/share/nvim/backup
-###
-exit 0
