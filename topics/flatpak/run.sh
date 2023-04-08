bc9ad74157f459bacff81e7b5633052e5dc5e149
Manuel Sanchez Pinar <aomanu@gmail.com>
2023-04-08 16:00:48 +0200

    update flatpak

--- topics/flatpak/run.sh
+++ topics/flatpak/run.sh
@@ -8,3 +8,6 @@ set -euo pipefail
 cd "$(dirname -- "$0")"
 
 ansible-playbook playbook.yml
+
+flatpak --user override --filesystem="$HOME/.local/share/fonts"
+flatpak --user override --filesystem="$HOME/.icons"
