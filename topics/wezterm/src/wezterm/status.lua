daee64f61af4921bdb7fa8926a949b0e6b08bd40
Manuel Sanchez Pinar <aomanu@gmail.com>
2023-04-22 21:07:58 +0200

    wezterm: update

--- topics/wezterm/src/wezterm/status.lua
+++ topics/wezterm/src/wezterm/status.lua
@@ -1,29 +1,42 @@
 local wezterm = require("wezterm")
 
 wezterm.on("update-status", function(window, pane)
+  local active_keytable = window:active_key_table()
+  if active_keytable then
+    window:set_left_status(wezterm.format({
+      { Foreground = { Color = "black" } },
+      { Attribute = { Intensity = "Bold" } },
+      { Background = { Color = "lightblue" } },
+      { Text = " " .. active_keytable .. " " },
+    }))
+    return
+  end
+
+  if window:leader_is_active() then
+    window:set_left_status(wezterm.format({
+      { Foreground = { Color = "#fe8019" } },
+      { Attribute = { Intensity = "Bold" } },
+      { Background = { Color = "yellow" } },
+      { Foreground = { Color = "black" } },
+      { Text = " Leader " },
+    }))
+    return
+  end
+
   window:set_left_status(wezterm.format({
-    { Background = { Color = "#fe8019" } },
-    { Foreground = { Color = "#000000" } },
+    { Foreground = { Color = "#fe8019" } },
+    { Attribute = { Intensity = "Bold" } },
     { Text = " " .. window:active_workspace() .. " " },
   }))
 end)
 
 wezterm.on("update-right-status", function(window, pane)
   local dim = pane:get_dimensions()
-  local leader = window:leader_is_active() and { Text = " P " } or { Text = "" }
-  local active_keytable = window:active_key_table()
-  local keytable = active_keytable and { Text = " " .. active_keytable .. " " } or { Text = "" }
 
   window:set_right_status(wezterm.format({
     { Background = { Color = "#504945" } },
     { Text = " " .. pane:get_title() },
     { Attribute = { Italic = true } },
     { Text = " " .. dim.scrollback_rows .. "L " },
-    { Attribute = { Intensity = "Bold" } },
-    { Background = { Color = "yellow" } },
-    { Foreground = { Color = "black" } },
-    leader,
-    { Background = { Color = "lightblue" } },
-    keytable,
   }))
 end)
