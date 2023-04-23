6d6d07db204c82b6d93b96aac4ef29ac470c864d
Manuel Sanchez Pinar <aomanu@gmail.com>
2023-04-23 20:51:32 +0200

    remove some aliases

--- topics/shell/etc/bash/aliases.bash
+++ topics/shell/etc/bash/aliases.bash
@@ -9,11 +9,6 @@ alias ls='ls --color=auto'
 alias diff='diff --color=auto'
 alias htop='sudo TERM=xterm-256color htop'
 
-# some more ls aliases
-alias ll='ls -alF'
-alias la='ls -A'
-alias l='ls -CF'
-
 alias grep='grep --color=auto'
 alias fgrep='fgrep --color=auto'
 alias egrep='egrep --color=auto'
