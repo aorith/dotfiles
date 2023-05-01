04dd52b4701a4f31496da6da2f7b3d0a4aad3142
Manuel Sanchez Pinar <aomanu@gmail.com>
2023-05-01 11:16:45 +0200

    cleanup

--- topics/git/init.sh
+++
@@ -1,9 +0,0 @@
-# vim: ft=bash
-
-### bootstrap
-[[ -d $PRIVATE_GITHOME ]] && create_link "${PRIVATE_GITHOME}" "${GITHOME}/00_SYNC" || true
-create_link "${PWD}/src/config/git" "$HOME/.config/git"
-create_link "${PWD}/src/config/tig" "$HOME/.config/tig"
-[[ -f "$HOME/.gitconfig" ]] && rm "$HOME/.gitconfig" || true
-###
-exit 0
