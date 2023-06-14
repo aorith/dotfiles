ca85537c9bbdf670ab52570fd58008a1a630f99a
Manuel Sanchez Pinar <aomanu@gmail.com>
2023-06-14 09:35:20 +0200

    add git topic

---
+++ topics/git/init.sh
@@ -0,0 +1,7 @@
+# vim: ft=bash
+
+[[ ! -d $PRIVATE_GITHOME ]] || create_link "${PRIVATE_GITHOME}" "${GITHOME}/private_githome"
+[[ ! -f "$HOME/.gitconfig" ]] || rm "$HOME/.gitconfig"
+
+create_link "${PWD}/src/config/git" "$HOME/.config/git"
+create_link "${PWD}/src/config/gitui" "$HOME/.config/git"
