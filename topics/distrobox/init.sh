f8d7255e9340afd2348b4ffbab65be4bb567839a
Manuel Sanchez Pinar <aomanu@gmail.com>
2023-07-04 12:02:48 +0200

    update

--- topics/distrobox/init.sh
+++ topics/distrobox/init.sh
@@ -1,6 +1,8 @@
 # vim: ft=bash
 
 [[ "$(uname -s)" == "Linux" ]] || exit "$_SKIP"
+type distrobox >/dev/null 2>&1 || { log_warn "distrobox is not installed"; exit "$_SKIP"; }
 
 create_link "$PWD/src/distrobox" "$HOME/.config/distrobox"
+distrobox assemble create --file "$HOME/githome/dotfiles/topics/distrobox/distrobox.ini"
 exit 0
