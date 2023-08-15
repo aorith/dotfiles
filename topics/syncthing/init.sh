4c29f1e2b7187f9271756de90b2187af40a8f44c
Manuel Sanchez Pinar <aomanu@gmail.com>
2023-08-15 09:32:31 +0200

    add syncthing topic

---
+++ topics/syncthing/init.sh
@@ -0,0 +1,16 @@
+# vim: ft=bash
+
+[[ -e ~/Syncthing ]] || exit $_SKIP
+
+log_info "Copying ignore files ..."
+
+base_folder=$(readlink ~/Syncthing)
+while read -r d; do
+    base_dir=$(basename -- "$d")
+    [[ "$base_dir" != "_config" ]] || continue
+
+    cp ./src/stignore "${d}/.stignore"
+    cp ./src/global-ignore "${d}/global-ignore"
+done < <(find "$base_folder" -mindepth 1 -maxdepth 1 -type d)
+
+exit 0
