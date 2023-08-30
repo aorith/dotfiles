b5e6266a0da26598cda91261e120fc0f277ffdbb
Manuel Sanchez Pinar <aomanu@gmail.com>
2023-08-30 07:56:44 +0200

    alacritty: update

--- topics/alacritty/src/alacritty/bin/switch-theme.sh
+++ topics/alacritty/src/alacritty/bin/switch-theme.sh
@@ -9,7 +9,6 @@ cd "$BASET" || exit 1
 if [[ -z "$1" ]]; then
     {
         find mine/ -type f \( -name "*.yml" -o -name "*.yaml" \) -not -name "schemes.yaml"
-        find zenbones/ -type f \( -name "*.yml" -o -name "*.yaml" \) -not -name "schemes.yaml"
     } | fzf --preview "head -n 100 {} && ln -sf ${BASET}/$(basename {}) ${HOME}/.config/alacritty/theme.yml && cp ${CONFIG} /tmp/.alacritty.yml && echo >> ${CONFIG} && mv /tmp/.alacritty.yml ${CONFIG}"
 else
     [[ "$(readlink -f "${HOME}"/.config/alacritty/theme.yml)" != "$(readlink -f "${BASET}/${1}")" ]] || exit 0
