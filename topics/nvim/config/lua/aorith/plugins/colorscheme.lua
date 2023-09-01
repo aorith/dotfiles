7d4c42bbaf9d7b5c2884ddb7e04e06849e580a27
Manuel Sanchez Pinar <aomanu@gmail.com>
2023-09-01 23:11:32 +0200

    nvim: update

--- topics/nvim/config/lua/aorith/plugins/colorscheme.lua
+++ topics/nvim/config/lua/aorith/plugins/colorscheme.lua
@@ -1,23 +1,39 @@
 return {
   {
-    "bluz71/vim-nightfly-colors",
+    'bluz71/vim-nightfly-colors',
     enabled = false,
-    name = "nightfly",
+    name = 'nightfly',
     lazy = false,
     priority = 1000,
     config = function()
-      vim.cmd("colorscheme nightfly")
+      vim.cmd('colorscheme nightfly')
     end,
   },
 
   {
-    "folke/tokyonight.nvim",
-    enabled = true,
+    'folke/tokyonight.nvim',
+    enabled = false,
     lazy = false,
     priority = 1000,
     opts = {},
     config = function()
-      vim.cmd("colorscheme tokyonight-moon")
+      vim.cmd('colorscheme tokyonight-moon')
+    end,
+  },
+
+  {
+    'rebelot/kanagawa.nvim',
+    enabled = true,
+    cmd = { 'KanagawaCompile' },
+    build = ':KanagawaCompile',
+    lazy = false,
+    priority = 1000,
+    config = function()
+      require('kanagawa').setup({
+        compile = true, -- enable compiling the colorscheme
+      })
+
+      vim.cmd('colorscheme kanagawa')
     end,
   },
 }
