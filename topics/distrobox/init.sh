0f1f8bb1939c6cd3ba1a1af5fb07ada42ec61e96
Manuel Sanchez Pinar <aomanu@gmail.com>
2023-09-17 11:10:42 +0200

    upd distrobox

--- topics/distrobox/init.sh
+++ topics/distrobox/init.sh
@@ -1,13 +1,11 @@
 # vim: ft=bash
 
 [[ "$(uname -s)" == "Linux" ]] || exit "$_SKIP"
-type distrobox >/dev/null 2>&1 || { log_warn "distrobox is not installed"; exit "$_SKIP"; }
+type distrobox >/dev/null 2>&1 || {
+    log_warn "distrobox is not installed"
+    exit "$_SKIP"
+}
 
 create_link "$PWD/src/distrobox" "$HOME/.config/distrobox"
 
-if [[ -d /etc/nixos ]]; then
-    distrobox assemble create --file "$HOME/githome/dotfiles/topics/distrobox/distrobox_nixos.ini"
-else
-    distrobox assemble create --file "$HOME/githome/dotfiles/topics/distrobox/distrobox.ini"
-fi
-exit 0
+distrobox assemble create --replace --file "$HOME/githome/dotfiles/topics/distrobox/distrobox.ini"
