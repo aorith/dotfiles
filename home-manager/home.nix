4b30b9d42e4431d9c7d297d47fa330778187e148
Manuel Sanchez Pinar <aomanu@gmail.com>
2023-09-17 18:50:33 +0200

    wip

--- home-manager/home.nix
+++ home-manager/home.nix
@@ -1,71 +1,75 @@
-{ config, pkgs, ... }:
-
 {
-  # Home Manager needs a bit of information about you and the paths it should
-  # manage.
-  home.username = "aorith";
-  home.homeDirectory = "/var/home/aorith";
+  config,
+  pkgs,
+  ...
+}: {
+  home = {
+    username = "aorith";
+    homeDirectory = "/var/home/aorith";
 
-  # This value determines the Home Manager release that your configuration is
-  # compatible with. This helps avoid breakage when a new Home Manager release
-  # introduces backwards incompatible changes.
-  #
-  # You should not change this value, even if you update Home Manager. If you do
-  # want to update the value, then make sure to first check the Home Manager
-  # release notes.
-  home.stateVersion = "23.05"; # Please read the comment before changing.
+    # The home.packages option allows you to install Nix packages into your
+    # environment.
+    packages = with pkgs; [
+      (nerdfonts.override {fonts = ["Hack" "JetBrainsMono" "IBMPlexMono" "DejaVuSansMono"];})
 
-  # The home.packages option allows you to install Nix packages into your
-  # environment.
-  home.packages = with pkgs; [
-    # # Adds the 'hello' command to your environment. It prints a friendly
-    # # "Hello, world!" when run.
-    # pkgs.hello
+      neovim
 
-    # # It is sometimes useful to fine-tune packages, for example, by applying
-    # # overrides. You can do that directly here, just don't forget the
-    # # parentheses. Maybe you want to install Nerd Fonts with a limited number of
-    # # fonts?
-    # (pkgs.nerdfonts.override { fonts = [ "FantasqueSansMono" ]; })
-    (nerdfonts.override { fonts = [ "Hack" "JetBrainsMono" "IBMPlexMono" "DejaVuSansMono" ]; })
+      # tools
+      fd
+      gitui
+      silver-searcher
+      terraform
 
-    # # You can also create simple shell scripts directly inside your
-    # # configuration. For example, this adds a command 'my-hello' to your
-    # # environment:
-    # (pkgs.writeShellScriptBin "my-hello" ''
-    #   echo "Hello, ${config.home.username}!"
-    # '')
-  ];
+      # formatters, linters, etc
+      alejandra
+      black
+      djlint
+      golangci-lint
+      gotools
+      isort
+      nixpkgs-fmt
+      nodePackages.prettier
+      prettierd
+      ruff
+      shellcheck
+      shfmt
+      stylua
+      yamlfmt
+      yamllint
 
-  # Home Manager is pretty good at managing dotfiles. The primary way to manage
-  # plain files is through 'home.file'.
-  home.file = {
-    # # Building this configuration will create a copy of 'dotfiles/screenrc' in
-    # # the Nix store. Activating the configuration will then make '~/.screenrc' a
-    # # symlink to the Nix store copy.
-    # ".screenrc".source = dotfiles/screenrc;
+      # language servers
+      emmet-ls
+      gopls
+      lua-language-server
+      marksman
+      nil
+      nodePackages.bash-language-server
+      nodePackages.pyright
+      nodePackages.typescript-language-server
+      nodePackages.yaml-language-server
+      ruff-lsp
+      sqls
+      terraform-ls
+      tflint
+      vscode-langservers-extracted
+    ];
 
-    # # You can also set the file content immediately.
-    # ".gradle/gradle.properties".text = ''
-    #   org.gradle.console=verbose
-    #   org.gradle.daemon.idletimeout=3600000
-    # '';
+    sessionVariables = {
+      EDITOR = "nvim";
+      VISUAL = "nvim";
+    };
+
+    stateVersion = "23.05"; # Please read the comment before changing.
   };
 
-  # You can also manage environment variables but you will have to manually
-  # source
-  #
-  #  ~/.nix-profile/etc/profile.d/hm-session-vars.sh
-  #
-  # or
-  #
-  #  /etc/profiles/per-user/aorith/etc/profile.d/hm-session-vars.sh
-  #
-  # if you don't want to manage your shell through Home Manager.
-  home.sessionVariables = {
-    EDITOR = "nvim";
+  programs = {
+    fzf.enable = true;
+    jq.enable = true;
+    ripgrep.enable = true;
   };
 
   # Let Home Manager install and manage itself.
   programs.home-manager.enable = true;
+  # Enable on non-nixos linux systems
+  targets.genericLinux.enable = true;
 }
