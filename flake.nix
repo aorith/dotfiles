6d8b30970fbdf9451e6a944e8b7c16a8e03fdc52
Manuel Sanchez Pinar <aomanu@gmail.com>
2023-09-17 18:49:39 +0200

    wip

---
+++ flake.nix
@@ -0,0 +1,28 @@
+{
+  description = "Home Manager configuration of aorith";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    home-manager = {
+      url = "github:nix-community/home-manager";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
+
+  outputs = {
+    nixpkgs,
+    home-manager,
+    ...
+  }: let
+    system = "x86_64-linux";
+    pkgs = nixpkgs.legacyPackages.${system};
+  in {
+    homeConfigurations.aorith = home-manager.lib.homeManagerConfiguration {
+      inherit pkgs;
+
+      modules = [./home-manager/home.nix];
+      # Optionally use extraSpecialArgs
+      # to pass through arguments to home.nix
+    };
+  };
+}
