ed535a915278afee7767bea11598e677d8cf21ac
Manuel Sanchez Pinar <aomanu@gmail.com>
2023-12-02 10:14:18 +0100

    flake update

--- topics/nvim/config/filetype.lua
+++ topics/nvim/config/filetype.lua
@@ -1,8 +1,8 @@
 vim.filetype.add({
   pattern = {
-    [".*.j2"] = "htmldjango",
-    [".*.jinja"] = "htmldjango",
-    [".*.jinja2"] = "htmldjango",
+    [".*html.j2"] = "htmldjango",
+    [".*html.jinja"] = "htmldjango",
+    [".*html.jinja2"] = "htmldjango",
     [".*.html"] = function(path, bufnr)
       local result = -1
       vim.api.nvim_buf_call(bufnr, function() result = vim.fn.search("end }}") end)
