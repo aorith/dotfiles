e4c1f18a7137c000292e8403976717d03278d929
Manuel Sanchez Pinar <aomanu@gmail.com>
2024-01-05 10:04:36 +0100

    alacritty: migrate to toml

--- topics/alacritty/init.sh
+++ topics/alacritty/init.sh
@@ -3,6 +3,6 @@
 ### bootstrap
 [[ -d "$HOME/.config/alacritty" ]] || mkdir -p "$HOME/.config/alacritty"
 create_link "${PWD}/src/alacritty" "$HOME/.config/alacritty"
-create_link "${PWD}/src/alacritty/$(uname -s).yml" "${PWD}/src/alacritty/env.yml"
+create_link "${PWD}/src/alacritty/$(uname -s).toml" "${PWD}/src/alacritty/env.toml"
 ###
 exit 0
--- topics/alacritty/src/alacritty/bin/switch-theme.sh
+++ topics/alacritty/src/alacritty/bin/switch-theme.sh
@@ -2,16 +2,16 @@
 set -e
 
 BASET="${HOME}/.config/alacritty/themes"
-CONFIG="${HOME}/.config/alacritty/alacritty.yml"
+CONFIG="${HOME}/.config/alacritty/alacritty.toml"
 
 cd "$BASET" || exit 1
 
 if [[ -z "$1" ]]; then
     {
-        find mine/ -type f \( -name "*.yml" -o -name "*.yaml" \) -not -name "schemes.yaml"
-    } | fzf --preview "head -n 100 {} && ln -sf ${BASET}/$(basename {}) ${HOME}/.config/alacritty/theme.yml && cp ${CONFIG} /tmp/.alacritty.yml && echo >> ${CONFIG} && mv /tmp/.alacritty.yml ${CONFIG}"
+        find mine -type f -name "*.toml" -not -name "schemes.yaml"
+    } | fzf --preview "head -n 100 {} && ln -sf ${BASET}/$(basename {}) ${HOME}/.config/alacritty/theme.toml && cp ${CONFIG} /tmp/.alacritty.toml && echo >> ${CONFIG} && mv /tmp/.alacritty.toml ${CONFIG}"
 else
-    [[ "$(readlink -f "${HOME}"/.config/alacritty/theme.yml)" != "$(readlink -f "${BASET}/${1}")" ]] || exit 0
-    ln -sf "${BASET}/${1}" "${HOME}"/.config/alacritty/theme.yml &&
-        cp "${CONFIG}" /tmp/.alacritty.yml && echo >>"${CONFIG}" && mv /tmp/.alacritty.yml "${CONFIG}"
+    [[ "$(readlink -f "${HOME}"/.config/alacritty/theme.toml)" != "$(readlink -f "${BASET}/${1}")" ]] || exit 0
+    ln -sf "${BASET}/${1}" "${HOME}"/.config/alacritty/theme.toml &&
+        cp "${CONFIG}" /tmp/.alacritty.toml && echo >>"${CONFIG}" && mv /tmp/.alacritty.toml "${CONFIG}"
 fi
