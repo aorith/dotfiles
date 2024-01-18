06e770ffcf94ea733ff886eb14a7827ca718f65d
Manuel Sanchez Pinar <aomanu@gmail.com>
2024-01-18 17:02:05 +0100

    update

--- topics/misc/init.sh
+++ topics/misc/init.sh
@@ -7,5 +7,5 @@ create_link "${PWD}/src/vale" "$HOME/.config/vale"
 mkdir -p "$HOME/.config/vale/styles"
 
 if type vale >/dev/null 2>&1; then
-    vale sync --config="$HOME/.config/vale/vale.ini"
+    (cd ~/.config/vale && vale sync)
 fi
