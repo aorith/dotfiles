dd7ddc1709cfe72958dde10ef0e7512091c9b373
Manuel Sanchez Pinar <aomanu@gmail.com>
2024-01-27 18:10:51 +0100

    nvim: update

--- topics/nvim/config/lua/aorith/plugins/theme.lua
+++ topics/nvim/config/lua/aorith/plugins/theme.lua
@@ -1,4 +1,4 @@
-local dark_theme = "kanagawa"
+local dark_theme = "randomhue"
 local light_theme = "catppuccin"
 
 local function detect_background()
@@ -26,6 +26,8 @@ return {
     lazy = false,
     priority = 1000,
     config = function()
+      if theme == "randomhue" then vim.cmd.colorscheme("randomhue") end
+
       if string.find(theme, "kanagawa") then
         require("kanagawa").setup({
           dimInactive = false,
@@ -52,6 +54,27 @@ return {
               ["@text.todo.checked.markdown"] = { bg = "NONE", fg = colors.theme.vcs.added },
               ["@text.todo.unchecked.markdown"] = { bg = "NONE", fg = colors.theme.vcs.changed },
               ["@text.strong.markdown_inline"] = { fg = colors.theme.syn.constant, bold = true },
+
+              -- update kanagawa to handle new treesitter highlight captures
+              ["@string.regexp"] = { link = "@string.regex" },
+              ["@variable.parameter"] = { link = "@parameter" },
+              ["@exception"] = { link = "@exception" },
+              ["@string.special.symbol"] = { link = "@symbol" },
+              ["@markup.strong"] = { link = "@text.strong" },
+              ["@markup.italic"] = { link = "@text.emphasis" },
+              ["@markup.heading"] = { link = "@text.title" },
+              ["@markup.raw"] = { link = "@text.literal" },
+              ["@markup.quote"] = { link = "@text.quote" },
+              ["@markup.math"] = { link = "@text.math" },
+              ["@markup.environment"] = { link = "@text.environment" },
+              ["@markup.environment.name"] = { link = "@text.environment.name" },
+              ["@markup.link.url"] = { link = "Special" },
+              ["@markup.link.label"] = { link = "Identifier" },
+              ["@comment.note"] = { link = "@text.note" },
+              ["@comment.warning"] = { link = "@text.warning" },
+              ["@comment.danger"] = { link = "@text.danger" },
+              ["@diff.plus"] = { link = "@text.diff.add" },
+              ["@diff.minus"] = { link = "@text.diff.delete" },
             }
           end,
         })
