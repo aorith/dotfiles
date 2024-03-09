f98cfc46d853d7dacb09720e23e03f408ce37ed4
Manuel Sanchez Pinar <aomanu@gmail.com>
2024-03-09 13:07:11 +0100

    remove vale

--- topics/misc/init.sh
+++ topics/misc/init.sh
@@ -5,15 +5,6 @@ if [[ ! -d /etc/nixos ]]; then
     create_link "${PWD}/src/nix" "$HOME/.config/nix"
 fi
 
-create_link "${PWD}/src/vale" "$HOME/.config/vale"
-mkdir -p "$HOME/.config/vale/styles"
-
-if type -f vale >/dev/null 2>&1; then
-    (cd ~/.config/vale && vale sync)
-elif type -f nix >/dev/null 2>&1; then
-    (cd ~/.config/vale && nix run nixpkgs#vale -- sync)
-fi
-
 # https://wiki.archlinux.org/title/PipeWire#Noticeable_audio_delay_or_audible_pop/crack_when_starting_playback
 case $OSTYPE in
 linux*)
