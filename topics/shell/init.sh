37acc7be21ff3748c810c6501f680a9011722200
Manuel Sanchez Pinar <aomanu@gmail.com>
2024-04-24 11:16:27 +0200

    update

--- topics/shell/init.sh
+++ topics/shell/init.sh
@@ -5,8 +5,11 @@ mkdir -p ~/.local/bin
 # BASH
 mkdir -p ~/.local/share/zsh
 create_link "${PWD}/src/inputrc" "$HOME/.inputrc"
-create_link "${PWD}/src/bash/bashrc" "$HOME/.bashrc"
-create_link "${PWD}/src/bash/bash_profile" "$HOME/.bash_profile"
+
+# Managed by nix
+#create_link "${PWD}/src/bash/bashrc" "$HOME/.bashrc"
+#create_link "${PWD}/src/bash/bash_profile" "$HOME/.bash_profile"
+
 source "${PWD}/etc/common/env.sh"
 if [[ -n "$GOBIN" ]]; then
     mkdir -p "$GOBIN"
