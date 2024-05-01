a4f8d2b6a3011c18d5553b910dc178aa19fd8867
Manuel Sanchez Pinar <aomanu@gmail.com>
2024-05-01 21:17:39 +0200

    update

--- topics/misc/init.sh
+++ topics/misc/init.sh
@@ -1,9 +1,7 @@
 # vim: ft=bash
 
 create_link "${PWD}/src/yamllint" "$HOME/.config/yamllint"
-if [[ ! -d /etc/nixos ]]; then
-    create_link "${PWD}/src/nix" "$HOME/.config/nix"
-fi
+#[[ ! -d /etc/nixos ]] || create_link "${PWD}/src/nix" "$HOME/.config/nix"
 
 # https://wiki.archlinux.org/title/PipeWire#Noticeable_audio_delay_or_audible_pop/crack_when_starting_playback
 case $OSTYPE in
