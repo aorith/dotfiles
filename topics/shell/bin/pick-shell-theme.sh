2b923eaccabd1d9202bbfdad0b0a36e8c9801c5a
Manuel Sanchez Pinar <aomanu@gmail.com>
2024-10-09 09:03:54 +0200

    force color in pick-shell-theme

--- topics/shell/bin/pick-shell-theme.sh
+++ topics/shell/bin/pick-shell-theme.sh
@@ -6,6 +6,6 @@ cd "$BASET" || exit 1
 
 {
     find . -type f -name "base*.sh"
-} | fzf --preview "bat {} && ln -sf '$BASET/$(basename {})' '$HOME/.config/tinted-shell-theme.sh'"
+} | fzf --preview "bat --color=always {} && ln -sf '$BASET/$(basename {})' '$HOME/.config/tinted-shell-theme.sh'"
 
 bash "$HOME/.config/tinted-shell-theme.sh"
