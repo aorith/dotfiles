3bc189ead7065c87d890fbf3ffe3265743f61d13
Manuel Sanchez Pinar <aomanu@gmail.com>
2024-10-09 12:03:06 +0200

    pick theme with tmux/fzf

--- topics/shell/bin/pick-shell-theme.sh
+++ topics/shell/bin/pick-shell-theme.sh
@@ -4,8 +4,16 @@ set -e
 BASET="$HOME/.config/tinted-theming/tinted-shell/scripts"
 cd "$BASET" || exit 1
 
+if [[ -n "$TMUX" ]]; then
+    # shellcheck disable=SC2155
+    current_tty="$(tmux display-message -p "#{pane_tty}")"
+else
+    # shellcheck disable=SC2155
+    current_tty="$(tty)"
+fi
+
 {
     find . -type f -name "base*.sh"
-} | fzf --preview "bat --color=always {} && ln -sf '$BASET/$(basename {})' '$HOME/.config/tinted-shell-theme.sh'"
+} | fzf --preview "TTY='$current_tty' bash '$BASET/$(basename {})' && '$HOME/Syncthing/SYNC_STUFF/githome/private_dotfiles/topics/scripts-private/bin/,colortest' && ln -sf '$BASET/$(basename {})' '$HOME/.config/tinted-shell-theme.sh'"
 
 bash "$HOME/.config/tinted-shell-theme.sh"
