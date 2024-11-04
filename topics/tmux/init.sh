325d184927cdfb1384f6d6a78c2312bdd50626e0
Manuel Sanchez Pinar <aomanu@gmail.com>
2024-11-04 21:02:35 +0100

    logging

--- topics/tmux/init.sh
+++ topics/tmux/init.sh
@@ -3,7 +3,11 @@
 create_link "${PWD}/src/tmux" "$HOME/.config/tmux"
 
 [[ -f "${PWD}/src/tmux/bin/tcdn_server_for_go/tcdn_server_for_go" ]] || {
-    type go >/dev/null 2>&1 || exit 0
+    log_info "Building 'tcdn_server_for_go' ..."
+    type go >/dev/null 2>&1 || {
+        log_warn "Go is not installed"
+        exit 0
+    }
 
     pushd "${PWD}/src/tmux/bin/tcdn_server_for_go/" >/dev/null &&
         ./build.sh &&
