a399d4a3a49732599f2f54c1f4ebf56be669ab6b
Manuel Sanchez Pinar <aomanu@gmail.com>
2024-11-16 09:12:08 +0100

    move python-cache out of project's folder

--- topics/shell/etc/common/env.sh
+++ topics/shell/etc/common/env.sh
@@ -16,6 +16,7 @@ export XDG_CACHE_HOME="$HOME/.cache"
 export XDG_CONFIG_HOME="$HOME/.config"
 export XDG_DATA_HOME="$HOME/.local/share"
 export XDG_STATE_HOME="$HOME/.local/state"
+export PYTHONPYCACHEPREFIX="/tmp/python-cache" # fine for sinlge-user systems
 
 # AA for java apps
 export _JAVA_OPTIONS="-Dawt.useSystemAAFontSettings=on"
