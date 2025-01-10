e119c525d365599f115dc4dd98029a6e48e3f2c6
Manuel Sanchez Pinar <aomanu@gmail.com>
2025-01-10 12:21:46 +0100

    cleanup functions

--- topics/shell/src/bash/functions.sh
+++ topics/shell/src/bash/functions.sh
@@ -9,38 +9,12 @@ dotfiles() {
 }
 
 repos() {
-    local gitdir _repo basepath
-    basepath="${HOME}/Syncthing/TES/gitlab"
-    [[ -n "$1" ]] || {
-        echo "Changing dir to ${basepath}"
-        cd "${basepath}" || return 1
-        return 0
-    }
-
-    while read -r -d '' gitdir; do
-        _repo=$(dirname -- "$gitdir")
-        if basename -- "$_repo" | grep -Eq "^${1}$"; then
-            echo "Found '$1' ... changing directory to '$_repo'."
-            cd "$_repo" || {
-                echo "ERROR: Could not change directory."
-                return 1
-            }
-            return 0
-        fi
-    done < <(find "${basepath}" -maxdepth 4 -type d -name '.git' -print0)
-
-    echo "ERROR: Unable to find repository: '$1'."
-    return 1
-}
-
-repos-fzf() {
     local p
-    p=$(fd \.git$ "$HOME/Syncthing/TES/gitlab" --max-depth 4 --type d --unrestricted --color never | fzf --delimiter / --with-nth -3,-4)
-    if [[ -d "$p" ]]; then
-        cd "${p}/.." || return 1
-    fi
+    p="$(fd \.git$ "${HOME}/storage/tank/data/TES/gitlab" --max-depth 4 --type d --unrestricted --color never | fzf --delimiter / --with-nth -3,-4)"
+    [[ -d "$p" ]] || return
+    cd "${p}/.." || return 1
 }
 
-# To manage k8s contexts
+# To manage k8s contexts, source the ,kc function here
 # shellcheck disable=SC1091
 source "${PRIVATE_DOTFILES}/topics/k8s/k8s-kc"
