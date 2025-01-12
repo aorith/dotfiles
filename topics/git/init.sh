feb90f7149d50b071fa51fb601ff8be72b66acbf
Manuel Sanchez Pinar <aomanu@gmail.com>
2025-01-12 10:17:36 +0100

    update

--- topics/git/init.sh
+++ topics/git/init.sh
@@ -1,6 +1,5 @@
 # vim: ft=bash
 
-[[ ! -d $PRIVATE_GITHOME ]] || create_link "${PRIVATE_GITHOME}" "${GITHOME}/private_githome"
 [[ ! -f "$HOME/.gitconfig" ]] || rm "$HOME/.gitconfig"
 
 create_link "${PWD}/src/config/git" "$HOME/.config/git"
