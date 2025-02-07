a636d984f14199d927e237a73f0a754ba519769f
Manuel Sanchez Pinar <aomanu@gmail.com>
2025-02-07 18:35:39 +0100

    update nvim conf

--- topics/neovim/nvim/lua/aorith/plugins/theme.lua
+++ topics/neovim/nvim/lua/aorith/plugins/theme.lua
@@ -1,12 +1,7 @@
 return {
-  "sainnhe/gruvbox-material",
+  "folke/tokyonight.nvim",
   lazy = false,
   priority = 1000,
-  config = function()
-    -- Optionally configure and load the colorscheme
-    -- directly inside the plugin declaration.
-    vim.g.gruvbox_material_enable_italic = true
-    vim.g.gruvbox_material_foreground = "original"
-    vim.cmd.colorscheme("gruvbox-material")
-  end,
+
+  config = function() vim.cmd.colorscheme("tokyonight") end,
 }
