1429b0e76bb03a6a280b7b9d2336ea868bfeb6f1
Manuel Sanchez Pinar <aomanu@gmail.com>
2025-03-30 11:58:41 +0200

    nvim: try virtual_lines

--- topics/neovim/nvim/lua/aorith/plugins/lsp.lua
+++ topics/neovim/nvim/lua/aorith/plugins/lsp.lua
@@ -1,12 +1,13 @@
 -- Log level
-vim.lsp.set_log_level(vim.log.levels.OFF)
+vim.lsp.set_log_level(vim.log.levels.WARN)
 
 -- Diagnostics
 vim.diagnostic.config({
   signs = { priority = 9999 },
   underline = true,
   update_in_insert = false, -- false so diags are updated on InsertLeave
-  virtual_text = { severity = { min = "ERROR" } },
+  virtual_text = { current_line = true, severity = { min = "INFO", max = "WARN" } },
+  virtual_lines = { current_line = true, severity = { min = "ERROR" } },
   severity_sort = true,
   float = {
     focusable = false,
