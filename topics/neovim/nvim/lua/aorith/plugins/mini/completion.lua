b031be0b810ecf2b03b5b5875057bfa80f3505b9
Manuel Sanchez Pinar <aomanu@gmail.com>
2025-05-14 09:45:19 +0200

    nvim: use mini keymap

--- topics/neovim/nvim/lua/aorith/plugins/mini/completion.lua
+++ topics/neovim/nvim/lua/aorith/plugins/mini/completion.lua
@@ -12,29 +12,9 @@ require("mini.completion").setup({
 
 vim.o.completeopt = "menuone,noselect,noinsert,popup,fuzzy"
 
--- More consistent behavior of `<CR>`
-local keys = {
-  ["cr"] = vim.keycode("<CR>"),
-  ["ctrl-y"] = vim.keycode("<C-y>"),
-  ["ctrl-y_cr"] = vim.keycode("<C-y><CR>"),
-}
-
-_G.cr_action = function()
-  if vim.fn.pumvisible() ~= 0 then
-    -- If popup is visible, confirm selected item or add new line otherwise
-    local item_selected = vim.fn.complete_info()["selected"] ~= -1
-    return item_selected and keys["ctrl-y"] or keys["ctrl-y_cr"]
-  else
-    -- If popup is not visible, use plain `<CR>`. You might want to customize
-    -- according to other plugins. For example, to use 'mini.pairs', replace
-    -- next line with `return require('mini.pairs').cr()`
-    return keys["cr"]
-  end
-end
-
-vim.keymap.set("i", "<CR>", "v:lua._G.cr_action()", { expr = true })
-
-vim.keymap.set("i", "<Tab>", [[pumvisible() ? "\<C-n>" : "\<Tab>"]], { expr = true })
-vim.keymap.set("i", "<S-Tab>", [[pumvisible() ? "\<C-p>" : "\<S-Tab>"]], { expr = true })
+local map_multistep = require("mini.keymap").map_multistep
+map_multistep("i", "<Tab>", { "pmenu_next" })
+map_multistep("i", "<S-Tab>", { "pmenu_prev" })
+map_multistep("i", "<CR>", { "pmenu_accept" })
 
 require("mini.icons").tweak_lsp_kind()
