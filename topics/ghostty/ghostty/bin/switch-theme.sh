92039348a2db459699210ad62b4f23e357718c60
Manuel Sanchez Pinar <aomanu@gmail.com>
2025-08-05 08:29:36 +0200

    update terminal config

--- topics/ghostty/ghostty/bin/switch-theme.sh
+++ topics/ghostty/ghostty/bin/switch-theme.sh
@@ -1,40 +1,27 @@
-#!/bin/bash
+#!/bin/sh
 
-THEMES=(
-    "theme = dark:Ghostty Default StyleDark,light:selenized-light"
-    "theme = dark:selenized-black,light:selenized-white"
-    "theme = selenized-dark"
-    "theme = selenized-black"
-    "theme = selenized-light"
-    "theme = selenized-white"
-    "# Default"
-)
+THEME_FILE="$HOME/.config/ghostty/theme"
+theme="$(
+    echo "# Default
+theme = dark:Ghostty Default StyleDark,light:selenized-light
+theme = dark:selenized-black,light:selenized-white
+theme = selenized-dark
+theme = selenized-black
+theme = selenized-light
+theme = selenized-white" |
+        fzf --layout=reverse --tmux
+)"
 
-theme_file="$HOME/.config/ghostty/theme"
+[ -n "$theme" ] || exit 0
 
-current_index=1
-if [ -r "$theme_file" ]; then
-    first_line="$(head -n 1 "$theme_file")"
-    if [[ "$first_line" =~ ^#\ ([0-9]+)$ ]]; then
-        current_index=${BASH_REMATCH[1]}
-    fi
-fi
+echo "$theme" >"$THEME_FILE"
 
-if [ "$current_index" -lt 1 ] || [ "$current_index" -gt ${#THEMES[@]} ]; then
-    current_index=1
-fi
-next_index=$(((current_index % ${#THEMES[@]}) + 1))
-next_value="${THEMES[next_index - 1]}"
-
-echo "# $next_index" >"$theme_file"
-echo "$next_value" >>"$theme_file"
-
-cat <<EOF | osascript >/dev/null 2>&1
+if command -v osascript >/dev/null 2>&1; then
+    cat <<EOF | osascript >/dev/null 2>&1
 tell application "System Events"
     tell process "Ghostty"
         click menu item "Reload Configuration" of menu "Ghostty" of menu bar item "Ghostty" of menu bar 1
     end tell
 end tell
 EOF
-
-tmux display-message "  $next_index/${#THEMES[@]}: $next_value"
+fi
