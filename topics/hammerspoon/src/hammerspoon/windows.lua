e990093a1b55c3348aa40e05b2606d604635b081
Manuel Sanchez Pinar <aomanu@gmail.com>
2025-08-10 19:27:15 +0200

    hammerspoon: simplify and allow jumping adjacent screens

--- topics/hammerspoon/src/hammerspoon/windows.lua
+++ topics/hammerspoon/src/hammerspoon/windows.lua
@@ -14,22 +14,28 @@ H.pos = {
   right = { x = HALF_GRID_SIZE, y = 0, w = HALF_GRID_SIZE, h = GRID_SIZE },
   top = { x = 0, y = 0, w = GRID_SIZE, h = HALF_GRID_SIZE },
   bottom = { x = 0, y = HALF_GRID_SIZE, w = GRID_SIZE, h = HALF_GRID_SIZE },
-  topLeft = { x = 0, y = 0, w = HALF_GRID_SIZE, h = HALF_GRID_SIZE },
-  topRight = { x = HALF_GRID_SIZE, y = 0, w = HALF_GRID_SIZE, h = HALF_GRID_SIZE },
-  bottomLeft = { x = 0, y = HALF_GRID_SIZE, w = HALF_GRID_SIZE, h = HALF_GRID_SIZE },
-  bottomRight = { x = HALF_GRID_SIZE, y = HALF_GRID_SIZE, w = HALF_GRID_SIZE, h = HALF_GRID_SIZE },
 }
 
-H.moveWindowToPosition = function(cell, window)
-  if window == nil then window = hs.window.focusedWindow() end
-  if window then
-    local screen = window:screen()
-    hs.grid.set(window, cell, screen)
+H.moveWindowToPosition = function(pos)
+  local window = hs.window.focusedWindow()
+  if not window then return end
+  local screen_id = window:screen():id()
+
+  if pos == H.pos.left then
+    hs.grid.pushWindowLeft(window)
+  elseif pos == H.pos.right then
+    hs.grid.pushWindowRight(window)
+  elseif pos == H.pos.top then
+    hs.grid.pushWindowUp(window)
+  elseif pos == H.pos.bottom then
+    hs.grid.pushWindowDown(window)
   end
+
+  if screen_id == window:screen():id() then hs.grid.set(window, pos, nil) end
 end
 
-H.windowMaximize = function(window)
-  if window == nil then window = hs.window.focusedWindow() end
+H.windowMaximize = function()
+  local window = hs.window.focusedWindow()
   if window then window:maximize() end
 end
 
@@ -38,8 +44,4 @@ hs.hotkey.bind(mod, "right", function() H.moveWindowToPosition(H.pos.right) end)
 hs.hotkey.bind(mod, "left", function() H.moveWindowToPosition(H.pos.left) end)
 hs.hotkey.bind(mod, "up", function() H.moveWindowToPosition(H.pos.top) end)
 hs.hotkey.bind(mod, "down", function() H.moveWindowToPosition(H.pos.bottom) end)
-hs.hotkey.bind(mod, "space", function() hs.grid.show() end)
--- hs.hotkey.bind(mod, "1", function() wm.moveWindowToPosition(wm.pos.topLeft) end)
--- hs.hotkey.bind(mod, "2", function() wm.moveWindowToPosition(wm.pos.topRight) end)
--- hs.hotkey.bind(mod, "3", function() wm.moveWindowToPosition(wm.pos.bottomLeft) end)
--- hs.hotkey.bind(mod, "4", function() wm.moveWindowToPosition(wm.pos.bottomRight) end)
+hs.hotkey.bind(mod, "space", function() hs.grid.show(nil, true) end)
