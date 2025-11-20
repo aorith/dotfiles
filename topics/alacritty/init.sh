796b1f1f8db0e0e83de5f1d18445297e1b5c3664
Manuel Sanchez Pinar <aomanu@gmail.com>
2025-11-20 11:03:49 +0100

    update

--- topics/alacritty/init.sh
+++ topics/alacritty/init.sh
@@ -1,7 +1,6 @@
 # vim: ft=bash
 
 ### bootstrap
-[[ -d "$HOME/.config/alacritty" ]] || mkdir -p "$HOME/.config/alacritty"
 create_link "${PWD}/src/alacritty" "$HOME/.config/alacritty"
 create_link "${PWD}/src/alacritty/$(uname -s).toml" "${PWD}/src/alacritty/env.toml"
 ###
