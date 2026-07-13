a509ec1611ba4d63915097802e7b85c96f5875fb
Manuel Sanchez Pinar <aomanu@gmail.com>
2026-07-13 09:00:44 +0200

    Update

--- topics/macos/init.sh
+++ topics/macos/init.sh
@@ -15,7 +15,7 @@ esac
 #cp "${PWD}/src/DefaultKeyBinding.Dict" "$HOME/Library/KeyBindings/DefaultKeyBinding.Dict"
 
 # KeyRepeat
-defaults write -g InitialKeyRepeat -int 13
+defaults write -g InitialKeyRepeat -int 15
 defaults write -g KeyRepeat -int 2
 
 # https://tonsky.me/blog/monitors/
